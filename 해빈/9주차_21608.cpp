c2e1901b2d188d64a3e19652b066dfed6a267295
Haebin <hbyeon@student.42seoul.kr>
2023-01-26 00:38:23 +0900

    [9주차] 스터디 코드 - 해빈

--- 해빈/9주차_21608.cpp
+++ 해빈/9주차_21608.cpp
@@ -10,21 +10,24 @@ int dy[4] = {0, 0, -1, 1};
 int n;
 
 // student[i] : i번째 앉힐 학생
-int student[400];
+int student[405];
 
 // prefer[i][j] : i번 학생이 j를 선호 하는지 여부 (T/F)
-bool prefer[400][400] = {false};
+bool prefer[405][405];
 
 // visited[i][j] : (i, j) 좌표에 앉은 학생
-int visited[20][20];
+int visited[25][25] = {0};
+
+
 
 bool cmp(pair<pair<int,int>,pair<int,int> > &a, pair<pair<int, int>, pair<int, int> > &b) {
+    //second.first = x, second.second = y
     if (a.first.first == b.first.first) {
         if (a.first.second == b.first.second) {
-            if (a.second.first == b.second.first) {
-                return a.second.second < b.second.second;
+            if (a.second.second == b.second.second) {
+                return a.second.first < b.second.first;
             }
-            return a.second.first < b.second.first;
+            return a.second.second < b.second.second;
         }
         return a.first.second > b.first.second;
     }
@@ -35,25 +38,31 @@ void sit(int idx)
 {
     int student_num = student[idx];
     vector<pair<pair<int, int>, pair<int, int> > > promising;
+
     // [0][0] 부터 [n-1][n-1]까지의 모든 자리 탐색
     // 인접한 4방향에 대해 좋아하는 학생의 수 & 빈 자리 Count
     // vector< <좋아하는 학생 수, 비어있던 자리 수>, <x좌표, y좌표> >;
+
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             if (visited[i][j]) continue;
             int prefer_student = 0; // 인접 학생 중 선호 학생 수
             int empty = 0;          //
             for(int k = 0; k < 4; k++) {
-                int nx = i + dx[i];
-                int ny = j + dy[i];
-                if (nx < 0 || nx >= n || ny < 0 || ny <= n) continue;   // 자리 밖 continue;
+                int nx = i + dx[k];
+                int ny = j + dy[k];
+                if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;   // 자리 밖 continue;
                 if (prefer[student_num][visited[nx][ny]]) prefer_student++;
-                else if (!visited[nx][ny]) empty++;
+                else if (visited[nx][ny] == 0)
+                    empty++;
             }
-            promising.push_back(make_pair(make_pair(prefer_student, empty), make_pair(i, j)));
         }
     }
-    sort(promising.begin(), promising.end());
+    sort(promising.begin(), promising.end(), cmp);
+
+    int promising_x = promising[0].second.first;
+    int promising_y = promising[0].second.second;
+    visited[promising_x][promising_y] = student_num;
 }
 
 int satisfaction()
@@ -63,14 +72,13 @@ int satisfaction()
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             int prefer_student = 0;
-            int empty = 0;
             for(int k = 0; k < 4; k++){
-                int nx = i + dx[i];
-                int ny = j + dy[i];
-                if (nx < 0 || nx >= n || ny < 0 || ny <= n) continue;   // 자리 밖 continue;
-                if (prefer[prefer_student][visited[nx][ny]]) prefer_student++;
-                res += prefer_student;
+                int nx = i + dx[k];
+                int ny = j + dy[k];
+                if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;   // 자리 밖 continue;
+                if (prefer[visited[i][j]][visited[nx][ny]]) prefer_student++;
             }
+            res += satisfaction[prefer_student];
         }
     }
     return (res);
@@ -83,7 +91,7 @@ int main() {
 
     for (int i = 0; i < cnt; i++) {
         cin >> student[i];
-        for (int j = 0; j < 5; j++) {
+        for (int j = 0; j < 4; j++) {
             int prefer_student;
             cin >> prefer_student;
             prefer[student[i]][prefer_student] = true;

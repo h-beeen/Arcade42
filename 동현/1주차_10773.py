23548a7cdc6bf20fc45efcc81c3c76f08841efa2
rheefine <rheefine@gmail.com>
2023-02-24 17:17:36 +0900

    [1주차] 스터디 코드 - 동현

---
+++ 동현/1주차_10773.py
@@ -0,0 +1,13 @@
+import sys
+k = int(sys.stdin.readline())
+money = []
+
+for i in range(k):
+    money.append(int(sys.stdin.readline().split()[0]))
+
+    if money[-1] == 0:
+        del money[-2:]
+    else:
+        continue
+
+print(sum(money))

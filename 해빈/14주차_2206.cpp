514d6507a24937676045bb5c5e220f1c0eb9038f
Haebin <hbyeon@student.42seoul.kr>
2023-03-09 18:58:04 +0900

    [14주차] 스터디 코드 - 해빈 (#91)
    * [14주차] 스터디 코드 - 해빈
    
    * [14주차] 스터디 코드 - 해빈

--- 해빈/14주차_2206.cpp
+++ 해빈/14주차_2206.cpp
@@ -3,8 +3,9 @@
 #include <algorithm>
 using namespace std;
 
+string s;
 int n, m, board[1005][1005];
-bool vis[1005][1005];
+bool vis[1005][1005][2];
 int dx[4] = {0, 0, -1, 1};
 int dy[4] = {1, -1, 0, 0};
 
@@ -20,9 +21,12 @@ int main(void)
     ios::sync_with_stdio(0); cin.tie(0);
 
     cin >> n >> m;
-    for(int x = 0; x < n; x++)
-        for(int y = 0; y < m; y++)
-            cin >> board[x][y];
+    for(int i = 0; i < n; i++){
+        cin >> s;
+        for(int j = 0; j < m; j++)
+            board[i][j] = s[j] - '0';
+    }
+
 
     queue <pos> Q;
     pos temp = {0, 0, 1, 0};
@@ -31,7 +35,7 @@ int main(void)
     while(!Q.empty())
     {
         temp = Q.front(); Q.pop();
-        if(temp.x == n && temp.y == m) {
+        if(temp.x == n-1 && temp.y == m-1) {
             cout << temp.distance;
             return 0;
         }
@@ -40,20 +44,20 @@ int main(void)
             int nx = temp.x + dx[dir];
             int ny = temp.y + dy[dir];
 
-            if(nx < 0 || ny < 0 || nx >= n || ny >= m && vis[nx][ny]) continue;
+            if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
 
             // 벽인데 부순 적이 없고, 방문한 적 없는 경우
-            if(board[nx][ny] == 1 && !temp.breaking == 0){
-                pos temp2 = {nx, ny, temp.distance + 1, 1};
+            if(board[nx][ny] == 1 && !temp.breaking && !vis[nx][ny][1]){
+                pos temp2 = {nx, ny, temp.distance + 1, true};
+                vis[nx][ny][1] = true;
                 Q.push(temp2);
-                continue;
             }
 
             // 일반 길인데 방문한 적 없는 경우
-            if(!board[nx][ny]){
+            if(!board[nx][ny] && !vis[nx][ny][temp.breaking]){
                 pos temp2 = {nx, ny, temp.distance + 1, temp.breaking};
+                vis[nx][ny][temp.breaking] = true;
                 Q.push(temp2);
-                continue;
             }
         }
     }

16d6d7cbc5889ed55f076bfd79827a17be646d30
rnawslgk <hamo35301@gmail.com>
2023-03-11 00:13:09 +0900

    주석추가

--- 최주현/3주차_05427_실패.java
+++ 최주현/3주차_05427_실패.java
@@ -5,6 +5,21 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+//실패 사유
+/*
+불과 상근이 둘다 동시에 bfs 진행된다.
+
+--> 같은 레벨에서 진행되는 순서는 입력에 따라 큐에 먼저 들어 간거 부터 진행될텐데
+  이제 불이 붙으려는 칸으로 이동할 수 없다. >> 상근이보다 불이 먼저 움직여야돼
+  상근이가 있는 칸에 불이 옮겨옴과 동시에 다른 칸으로 이동할 수 있다. >> 불보다 상근이가 먼저 움직여야돼
+>> ????
+
+탈출 못하는 경우 >> ????
+
+상근이가 벽으로 가는 최단루트 --> level 계산
+ */
+
+//이하 코드는 그냥 bfs틀만 잡혀있다. 나중에 다시 풀때 입력부분 참고 예정
 public class Main {
 
     static int[][] board;

8dd1a0dfdcf74f1deaf54a647415893bfc621202
SWARVY <swarvy0826@naver.com>
2023-03-22 16:32:31 +0900

    edit : 주석 수정

--- 현호/5주차_11399.js
+++ 현호/5주차_11399.js
@@ -5,9 +5,12 @@ const size = Number(input.shift());
 function app() {
   // 입력 파싱
   const line = input[0].split(" ").map(Number);
+  // 총합 계산을 위한 변수
   let sum = 0;
 
+  // 배열 정렬
   line.sort((a, b) => a - b);
+  // 값을 더하는 반복문
   for (let i = 0; i < size; i++) {
     for (let j = 0; j <= i; j++) sum += line[j];
   }

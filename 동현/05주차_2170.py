c330dec2f1458e97e50c9c9828a60a092e901ff4
rheefine <rheefine@gmail.com>
2023-03-22 15:19:41 +0900

    5주차 선긋기

--- 동현/05주차_2170.py
+++ 동현/05주차_2170.py
@@ -9,17 +9,16 @@ xy.sort(key = lambda x: x[0])
 line = [xy[0]]
 
 for i in range(1,n):
-    if xy[i-1][1] >= xy[i][0]:
-        line[-1].append(max(xy[i-1][1],xy[i][1]))
-        line[-1].pop(1)
+    if xy[i-1][-1] >= xy[i][0]:
+        y_max = max(xy[i-1][-1],xy[i][-1])
+        xy[i].append(y_max)
+        line[-1].append(y_max)
     else:
         line.append(xy[i])
- 
+
 result = 0
 
-print(line)
-
 for i in range(len(line)):
-    result += line[i][1]-line[i][0]
+    result += line[i][-1]-line[i][0]
 
 print(result)

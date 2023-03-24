7b5a8ea53b2ed130b82e3250f63f1d4ec503f794
rheefine <rheefine@gmail.com>
2023-03-25 02:06:50 +0900

    [5주차] 스터디 코드 - 동현

--- 동현/05주차_2170.py
+++ 동현/05주차_2170.py
@@ -12,12 +12,12 @@ line = [xy[0]]
 
 for i in range(1,n):
     # 현재 y값이 다음 x 값보다 크거나 같으면
-    if xy[i-1][-1] >= xy[i][0]:
+    if xy[i-1][1] >= xy[i][0]:
         # 현재 y 값과 다음 y값 중 더 큰 값을 다음 y 값에 추가
-        y_max = max(xy[i-1][-1],xy[i][-1])
-        xy[i].append(y_max)
+        y_max = max(xy[i-1][1],xy[i][1])
+        xy[i][1]= y_max
         # line list에 있는 y 값 역시 변경
-        line[-1].append(y_max)
+        line[-1][1]= y_max
     # 현재 y 값이 다음 x 값보다 작으면 line에 다음 x,y 추가
     else:
         line.append(xy[i])

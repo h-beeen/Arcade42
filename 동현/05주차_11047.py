758758f6a83f54f57366beba38d932d3b4b62655
rheefine <rheefine@gmail.com>
2023-03-25 01:19:41 +0900

    [5주차] 스터디 코드 - 동현

--- 동현/05주차_11047.py
+++ 동현/05주차_11047.py
@@ -1,12 +1,19 @@
+# 동전
+
 from sys import stdin
 
+# n, k, coin 입력
 n, k = map(int,stdin.readline().split())
 coin = [int(stdin.readline()) for _ in range(n)]
 
+# 결과 값 선언
 result = 0
 
 for i in range(n-1,-1,-1):
+    # 값이 큰 coin부터 차례대로 나눈 후 몫을 결과값에 추가
     result += k//coin[i]
+    # k에 나머지 초기화
     k %= coin[i]
 
+# 결과 출력
 print(result)
--- 동현/05주차_2170.py
+++ 동현/05주차_2170.py
@@ -1,23 +1,31 @@
 from sys import stdin
 
+# n, xy 입력
 n = int(stdin.readline())
-
 xy = [list(map(int, stdin.readline().split())) for _ in range(n)]
 
+# x, y를 x를 기준으로 정렬
 xy.sort(key = lambda x: x[0])
 
+# 가장 작은 x,y list에 저장
 line = [xy[0]]
 
 for i in range(1,n):
+    # 현재 y값이 다음 x 값보다 크거나 같으면
     if xy[i-1][-1] >= xy[i][0]:
+        # 현재 y 값과 다음 y값 중 더 큰 값을 다음 y 값에 추가
         y_max = max(xy[i-1][-1],xy[i][-1])
         xy[i].append(y_max)
+        # line list에 있는 y 값 역시 변경
         line[-1].append(y_max)
+    # 현재 y 값이 다음 x 값보다 작으면 line에 다음 x,y 추가
     else:
         line.append(xy[i])
 
+# 결과 값 선언
 result = 0
 
+# 각각의 line 길이 계산 후 결과 값에 저장
 for i in range(len(line)):
     result += line[i][-1]-line[i][0]
 

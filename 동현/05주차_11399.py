d25979a3d52a6f1675fc0f4d573191b6feddd3b5
Rhee DongHyun <rheefine@gmail.com>
2023-03-27 11:36:41 +0900

    [5주차] 스터디 코드 - 동현 (#101)
    * [1주차] 스터디 코드 - 동현
    
    * [1,2주차] 스터디 코드 - 동현
    
    * [3주차] 스터디 코드 (주석 미포함) - 동현
    
    * [3주차] 스터디 코드 - 동현
    
    * [3주차] 스터디 코드 - 동현
    
    * [3주차] 스터디 코드 - 동현
    
    * [4주차] 스터디 코드 - 동현
    
    * [4주차] 스터디 코드 - 동현
    
    * 5주차 동전
    
    * 5주차 atm
    
    * 5주차 선긋기
    
    * 5주차 선긋기
    
    * [5주차] 스터디 코드 - 동현
    
    * [5주차] 스터디 코드 - 동현

---
+++ 동현/05주차_11399.py
@@ -0,0 +1,20 @@
+# ATM
+from sys import stdin
+
+# n, time 입력
+n = int(stdin.readline())
+time = list(map(int,stdin.readline().split()))
+
+# time 정렬
+time.sort()
+
+# 결과 값 선언
+result = 0
+
+for i in range(n):
+    # 각 시간이 나온 횟수를 곱하여 결과 값에 저장
+    result += time[i] * n
+    n -= 1
+
+# 결과 값 출력
+print(result)

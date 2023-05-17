7b2741bebe53c789abc7cea16b18cc67da56644e
SWARVY <swarvy0826@naver.com>
2023-05-17 16:20:22 +0900

    feat : 신현호 6주차 풀이 업로드

---
+++ 현호/6주차_11650.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const size = Number(input.shift());
+let numbers = input.filter((val, idx) => idx >= 0).map((val) => val.split(" "));
+let answer = "";
+
+numbers = numbers.sort((a, b) => {
+  if (a[0] === b[0])
+    return a[1] - b[1];
+  return a[0] - b[0];
+});
+numbers.forEach((value) => {
+  answer += `${value[0]} ${value[1]}\n`;
+});
+
+console.log(answer);
